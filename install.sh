d4bb366d0945e36fe9b4c1e7cc6b1f1791a5dbe3
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2024-11-08 13:14:56 -0800

    chore: update installation script

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+pip install ipykernel openai pandas requests torch torchaudio torchvision
+# Install Segment Anything
+pip install git+https://github.com/facebookresearch/segment-anything.git
+pip install opencv-python pycocotools matplotlib onnxruntime onnx
+# Download model checkpoint
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-pip install ipykernel matplotlib openai opencv-python pandas requests segment_anything torchaudio torchvision
