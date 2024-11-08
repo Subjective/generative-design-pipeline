bf648c32fb5c1d785d3b5c78605fac70570294f8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2024-11-08 13:06:10 -0800

    chore: add installation script

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+pip install ipykernel matplotlib openai opencv-python pandas requests segment_anything torchaudio torchvision
