fec1506f3efbf5d29b367c57cae317b067161f4b
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2025-01-15 10:00:23 -0800

    chore: add CLIP to install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ pip install git+https://github.com/facebookresearch/segment-anything.git
 pip install opencv-python pycocotools matplotlib onnxruntime onnx
 # Download model checkpoint
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+# Install CLIP
+pip install ftfy regex tqdm
+pip install git+https://github.com/openai/CLIP.git
